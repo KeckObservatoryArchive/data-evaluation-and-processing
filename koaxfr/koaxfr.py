8df88d2da9cf64597f45c832b9af61ba4e36deb8
Jeff Mader <jeff.mader@gmail.com>
2017-08-22 18:35:23 -1000

    Initial

---
+++ koaxfr/koaxfr.py
@@ -0,0 +1,14 @@
+def koaxfr(instr, utdate, directory, tpx=tpx):
+	"""
+	"""
+	
+	verify_instrument()
+	verify_date(utDate)
+	assert directory != '', 'directory value is blank'
+
+	# Transfer to NExScI
+	
+	# Send email to NExScI
+	
+	# Update database
+	
