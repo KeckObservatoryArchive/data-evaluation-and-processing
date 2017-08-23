bd221019134cec38872d3541ff268938e2d02d9a
Jeff Mader <jeff.mader@gmail.com>
2017-08-22 18:35:52 -1000

    Initial

---
+++ dep-obtain/dep_obtain.py
@@ -0,0 +1,16 @@
+def dep_obtain(instr, utDate, stageDir):
+	"""
+	"""
+	
+	verify_instrument()
+	verify_date(utDate)
+	assert stageDir != '', 'stageDir value is blank'
+	
+	# Make the staging directory
+	
+	# Get HST from utDate
+	
+	# Query the telescope schedule
+	
+	# Create stageDir/dep_obtainINSTR.txt
+	
