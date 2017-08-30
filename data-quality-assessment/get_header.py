4dd68f1ab9666c818ca7a792f3ac6490aaaead5c
koaadmin <koaadmin@keck.hawaii.edu>
2017-08-30 12:45:21 -0400

    Added keywords

--- data-quality-assessment/get_header.py
+++ data-quality-assessment/get_header.py
@@ -3,13 +3,21 @@ from astropy.io import fits
 class HDU():
 	def __init__(self, file):
 		self.data = fits.getdata(file)
-		self.header = fits.getheader(file)
+		self.keywords = fits.getheader(file)
 		
 		# all common keywords
+		self.instr = self.keywords['INSTRUME']
+		self.ra = self.keywords['RA']
+		self.dec = self.keywords['DEC']
+		self.naxis1 = self.keywords['NAXIS1']
+		self.naxis2 = self.keywords['NAXIS2']
+		self.binning = self.keywords['BINNING']
 
 	# instrument specific kewywords
 	def kcwi(self):
-		self.camera = self.header['CAMERA']
+		self.camera = self.keywords['CAMERA']
+		self.parantel = self.keywords['PARATEL']
+		self.parang = self.keywords['PARANG']
 
 	def nirspec(self):
 		pass
