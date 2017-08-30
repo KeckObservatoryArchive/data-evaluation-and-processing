871769628e26aa7d438bf288c5ef5d6976c5fae3
koaadmin <koaadmin@keck.hawaii.edu>
2017-08-30 13:57:56 -0400

    Inital

---
+++ data-quality-assessment/dqa_run.py
@@ -0,0 +1,21 @@
+import get_header
+from glob import glob
+impot kcwi
+
+files = glob('*.fits*')
+for file in files:
+    header = get_header.HDU(file)
+    instr = header.instr
+    
+    # Instrument specific functions
+    if instr == 'NIRSPEC':
+        header.nirspec()
+    elif instr == 'KCWI':
+        header.kcwi()
+        kcwi.make_jpg(file)
+        stats = kcwi.image_stats(header.data, header.naxis1, header.naxis2)
+        # kcwi.wcs(...)
+        # kcwi.config(...)        
+    else:
+        # Do something
+        pass
