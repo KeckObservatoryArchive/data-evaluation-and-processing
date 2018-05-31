a0d4381b9c19cac4bae4bff5ad5b9dcffd03edbe
Josh Riley <joshjriley@gmail.com>
2018-05-30 17:10:26 -1000

    streamline code

--- dep_obtain.py
+++ dep_obtain.py
@@ -1,93 +1,64 @@
 from datetime import datetime, timedelta
-from pytz import timezone
 from verification import *
 import os
 import json
 import urllib.request
+from common import url_get
 
 def dep_obtain(instrObj):
     """
-    Queries the telescope schedule database and creates the following files
-    in stageDir:
+    Queries the telescope schedule database and creates the following files in stageDir:
 
     dep_obtainINSTR.txt
-    dep_notschedINSTR.txt (if no entries fouund in database)
+    dep_notschedINSTR.txt (if no entries found in database)
 
     @param instrObj: the instrument object
     @type instrObj: instrument class
     """
 
-    # Verify the supplied instrument and UT date are allowed values/formats
-
-    #verify_instrument(instrObj.instr)
-    #verify_date(instrObj.utDate)
-
-    instrObj.log.info('dep_obtain: started for {} {} UT'.format(instrObj.instr, instrObj.utDate))
-
-    # Verify that the stage directory exists.  If not, then make it.
-
-    if not os.path.isdir(instrObj.dirs['stage']):
-        instrObj.log.info('dep_obtain: making stage directory {}'.format(stageDir))
-        os.makedirs(instrObj.dirs['stage'])
+    log = instrObj.log
+    log.info('dep_obtain: started for {} {} UT'.format(instrObj.instr, instrObj.utDate))
 
     # Get HST from utDate
-
+    
     utDateObj = datetime.strptime(instrObj.utDate, '%Y-%m-%d')
     hstDateObj = utDateObj - timedelta(days=1)
     hstDate = hstDateObj.strftime('%Y-%m-%d')
 
-    # URL to query telescope schedule
-
-    #url = ('https://www.keck.hawaii.edu/software/db_api/telSchedule.php', '?')
-    schedUrl = (instrObj.telUrl, 'cmd=getSchedule', '&date=', hstDate, '&instr=', instrObj.instr)
-    schedUrl = ''.join(schedUrl)
-
     # Output files
 
-    notScheduledFile = (instrObj.dirs['stage'], '/dep_notsched', instrObj.instr, '.txt')
-    notScheduledFile = ''.join(notScheduledFile)
-    obtainFile = (instrObj.dirs['stage'], '/dep_obtain', instrObj.instr, '.txt')
-    obtainFile = ''.join(obtainFile)
+    notScheduledFile = ''.join((instrObj.dirs['stage'], '/dep_notsched', instrObj.instr, '.txt'))
+    obtainFile       = ''.join((instrObj.dirs['stage'], '/dep_obtain', instrObj.instr, '.txt'))
 
     try:
-        instrObj.log.info('dep_obtain: retrieving telescope schedule information for {}'.format(instrObj.instr))
 
-        # Read the input URL
+        # Read the telescope schedul URL
 
-        data = urllib.request.urlopen(schedUrl)
-        data = data.read().decode('utf8')    # Convert from byte to ascii
-        if len(data) > 0:
-            data = json.loads(data)            # Convert to Python list
-        if isinstance(data, dict):
-            data = [data]
-
-        # Get the telescope number
-
-        telnr = instrObj.get_telnr()
+        schedUrl = ''.join((instrObj.telUrl, 'cmd=getSchedule', '&date=', hstDate, '&instr=', instrObj.instr))
+        log.info('dep_obtain: retrieving telescope schedule info: {}'.format(schedUrl))
+        schedData = url_get(schedUrl)
+        if isinstance(schedData, dict): schedData = [schedData]
 
         # Get OA
 
-        oaUrl = (instrObj.telUrl, 'cmd=getNightStaff', '&date=', hstDate)
-        oaUrl = oaUrl + ('&telnr=', str(telnr), '&type=oa')
-        oaUrl = ''.join(oaUrl)
-        oaGet = urllib.request.urlopen(oaUrl)
-        oaGet = oaGet.read().decode('utf8')
+        telnr = instrObj.get_telnr()
+        oaUrl = ''.join((instrObj.telUrl, 'cmd=getNightStaff', '&date=', hstDate, '&telnr=', str(telnr), '&type=oa'))
+        log.info('dep_obtain: retrieving night staff info: {}'.format(oaUrl))
+        oaData = url_get(oaUrl)
         oa = 'None'
-        if len(oaGet) > 0:
-            oaGet = json.loads(oaGet)
-            if isinstance(oaGet, dict):
-                if ('Alias' in oaGet):
-                    oa = oaGet['Alias']
+        if oaData:
+            if isinstance(oaData, dict):
+                if ('Alias' in oaData):
+                    oa = oaData['Alias']
             else:
-                for entry in oaGet:
+                for entry in oaData:
                     if entry['Type'] == 'oa':
                         oa = entry['Alias']
 
-        # No entries found
-        # Create stageDir/dep_notschedINSTR.txt and dep_obtainINSTR.txt
+        # No entries found: Create stageDir/dep_notschedINSTR.txt and dep_obtainINSTR.txt
 
-        if len(data) == 0:
-            instrObj.log.info('dep_obtain: no information found for {}'.format(instrObj.instr))
+        if (schedData == None):
+            log.info('dep_obtain: no telescope schedule info found for {}'.format(instrObj.instr))
 
             with open(notScheduledFile, 'w') as fp:
                 fp.write('{} not scheduled'.format(instrObj.instr))
@@ -95,36 +66,29 @@ def dep_obtain(instrObj):
             with open(obtainFile, 'w') as fp:
                 fp.write('{} {} NONE NONE NONE NONE NONE'.format(hstDate, oa))
 
-        # Entries found
-        # Create stageDir/dep_obtainINSTR.txt
+        # Entries found: Create stageDir/dep_obtainINSTR.txt
 
         else:
             with open(obtainFile, 'w') as fp:
                 num = 0
-                for entry in data:
+                for entry in schedData:
 
-                    # Get observer list from URL
+                    obsUrl = ''.join((instrObj.telUrl, 'cmd=getObservers', '&schedid=', entry['SchedId']))
+                    log.info('dep_obtain: retrieving observers info: {}'.format(obsUrl))
+                    obsData = url_get(obsUrl)
 
-                    obsUrl = (instrObj.telUrl, 'cmd=getObservers', '&schedid=', entry['SchedId'])
-                    obsUrl = ''.join(obsUrl)
-                    observer = urllib.request.urlopen(obsUrl)
-                    observer = observer.read().decode('utf8')
-                    observer = json.loads(observer)
-                    observers = 'None'
-                    if len(observer) > 0:
-                        observers = observer[0]['Observers']
+                    if obsData and len(obsData) > 0: observers = obsData[0]['Observers']
+                    else                           : observers = 'None'
 
-                    if num > 0:
-                        fp.write('\n')
-
+                    if num > 0: fp.write('\n')
                     fp.write('{} {} {} {} {} {} {}'.format(hstDate, oa, entry['Account'], entry['Institution'], entry['Principal'], entry['ProjCode'], observers))
 
-                    instrObj.log.info('dep_obtain: {} {} {} {} {} {} {}'.format(hstDate, oa, entry['Account'], entry['Institution'], entry['Principal'], entry['ProjCode'], observers))
+                    log.info('dep_obtain: {} {} {} {} {} {} {}'.format(hstDate, oa, entry['Account'], entry['Institution'], entry['Principal'], entry['ProjCode'], observers))
 
                     num += 1
 
     except:
-        instrObj.log.info('dep_obtain: {} error reading telescope schedule'.format(instrObj.instr))
+        log.info('dep_obtain: {} error reading telescope schedule'.format(instrObj.instr))
         return False
 
     return True
