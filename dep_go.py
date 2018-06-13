4ac049021b7ed55ccaa72a96fc1794898017c89b
Josh Riley <joshjriley@gmail.com>
2018-06-13 13:12:55 -1000

    Added optional process start and stop args to dep_go.py for extra convenience

--- dep_go.py
+++ dep_go.py
@@ -17,11 +17,15 @@ parser = argparse.ArgumentParser(description='DEP input parameters')
 parser.add_argument('instr', type=str, help='Instrument name')
 parser.add_argument('utDate', type=str, nargs='?', default=None, help='UTC Date to search for FITS files in prior 24 hours.')
 parser.add_argument('tpx', type=int, nargs='?', default=1, help='Update TPX databse?')
+parser.add_argument('processStart', type=str, nargs='?', default=None, help='Name of process to start.  Default is "obtain"')
+parser.add_argument('processStop', type=str, nargs='?', default=None, help='Name of process to stop.  Default is "koaxfr"')
 args = parser.parse_args()
 
 instr  = args.instr.upper()
 utDate = args.utDate
 tpx    = args.tpx
+pstart = args.processStart
+pstop  = args.processStop
 
 # Use the current UT date if none provided
 
@@ -31,6 +35,6 @@ if (utDate == None): utDate = datetime.utcnow().strftime('%Y-%m-%d')
 
 try:
 	dep = Dep(instr, utDate, config[instr]['ROOTDIR'], tpx)
-	dep.go()
+	dep.go(pstart, pstop)
 except Exception as error:
     do_fatal_error(repr(error), instr, utDate, 'dep_go')
