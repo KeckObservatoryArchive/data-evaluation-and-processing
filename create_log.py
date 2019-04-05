98b7344bd14bf5dec3eba7c3d6f2baacf9ed3cb2
Josh Riley <joshjriley@gmail.com>
2019-04-04 14:31:36 -1000

    converted indentation to spaces to be consisten with rest of project

--- create_log.py
+++ create_log.py
@@ -3,56 +3,56 @@ import os
 import sys
 
 def create_log(rootDir, instr, utDate, doStdout=None):
-	"""
-	Creates and returns a log file handler
+    """
+    Creates and returns a log file handler
 
-	@param rootDir: top directory where processed data will be written
-	@type rootDir: string
-	@param instr: instrument name
-	@type instr: string
-	@param utDate: UT date of observation
-	@type utDate: string (yyyy-mm-dd)
-	"""
+    @param rootDir: top directory where processed data will be written
+    @type rootDir: string
+    @param instr: instrument name
+    @type instr: string
+    @param utDate: UT date of observation
+    @type utDate: string (yyyy-mm-dd)
+    """
 
-	utDate = utDate.replace('/', '-')
-	utDateDir = utDate.replace('-', '')
+    utDate = utDate.replace('/', '-')
+    utDateDir = utDate.replace('-', '')
 
-	# Where to create the log (rootDir/instr)
+    # Where to create the log (rootDir/instr)
 
-	processDir = ''.join((rootDir, '/', instr.upper()))
+    processDir = ''.join((rootDir, '/', instr.upper()))
 
-	# Setup logging
+    # Setup logging
 
-	user = os.getlogin()
-	writerName = ''.join(('dep <', user, '>'))
-	log_writer = lg.getLogger(writerName)
-	log_writer.setLevel(lg.INFO)
+    user = os.getlogin()
+    writerName = ''.join(('dep <', user, '>'))
+    log_writer = lg.getLogger(writerName)
+    log_writer.setLevel(lg.INFO)
 
-	# Create a file handler
+    # Create a file handler
 
-	logFile = ''.join((processDir, '/dep_', instr.upper(), '_', utDateDir, '.log'))
-	log_handler = lg.FileHandler(logFile)
-	log_handler.setLevel(lg.INFO)
+    logFile = ''.join((processDir, '/dep_', instr.upper(), '_', utDateDir, '.log'))
+    log_handler = lg.FileHandler(logFile)
+    log_handler.setLevel(lg.INFO)
 
-	# Create logging format
+    # Create logging format
 
-	formatter = lg.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
-	log_handler.setFormatter(formatter)
+    formatter = lg.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+    log_handler.setFormatter(formatter)
 
-	# Add handler to the logger
+    # Add handler to the logger
 
-	log_writer.addHandler(log_handler)
+    log_writer.addHandler(log_handler)
 
-	#add stdout to output so we don't need both log and print statements(>= warning only)
+    #add stdout to output so we don't need both log and print statements(>= warning only)
 
-	if (doStdout):
-		sh = lg.StreamHandler(sys.stdout)
-		sh.setLevel(lg.WARNING)
-		formatter = lg.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-		sh.setFormatter(formatter)
-		log_writer.addHandler(sh)
-	
-	#init message and return
+    if (doStdout):
+        sh = lg.StreamHandler(sys.stdout)
+        sh.setLevel(lg.WARNING)
+        formatter = lg.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        sh.setFormatter(formatter)
+        log_writer.addHandler(sh)
+    
+    #init message and return
 
-	log_writer.info('create_log.py log_writer created')
-	return log_writer
+    log_writer.info('create_log.py log_writer created')
+    return log_writer
