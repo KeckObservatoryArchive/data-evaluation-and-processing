519d02dbd952579de1c4b58afbf02f6ef9334ce2
Josh Riley <joshjriley@gmail.com>
2019-09-17 16:17:29 -1000

    First working instr_kcwi thru dqa.  still needs lots of work.

--- instr_kcwi.py
+++ instr_kcwi.py
@@ -7,93 +7,142 @@ KCWI specific DR techniques can be added to it in the future
 
 import instrument
 import datetime as dt
+from common import *
 
 class Kcwi(instrument.Instrument):
-    def __init__(self, endTime=dt.datetime.now(), rDir=''):
+
+    def __init__(self, instr, utDate, rootDir, log=None):
+
         # Call the parent init to get all the shared variables
-        super().__init__(endTime, rDir)
-
-        # KCWI has the original file name
-        self.ofName = 'OFNAME'
-        self.camera = 'CAMERA'
-        self.endHour = 'DATE-END'
-        # Set the KCWI specific paths to anc and stage
-        seq = (self.rootDir, '/KCWI/', self.utDate, '/anc')
-        self.ancDir = ''.join(seq)
-        seq = (self.rootDir, '/stage')
-        self.stageDir = ''.join(seq)
-        # Generate the paths to the KCWI datadisk accounts
-        self.paths = self.get_dir_list()
-
-        def run_dqa_checks(self, progData):
-            '''
-            Run all DQA checks unique to this instrument.
-            '''
-
-            # todo: check that all of these do not need a subclass version if base class func was used.
-            ok = True
-            if ok: ok = self.set_instr()
-            if ok: ok = self.set_dateObs()
-            if ok: ok = self.set_utc()
-            self.get_dispmode(update=True)
-            self.get_camera(update=True)
-            if ok: ok = self.set_koaimtyp()
-            if ok: ok = self.set_koaid()
-            if ok: ok = self.set_ut()
-            if ok: ok = self.set_frameno()
-            if ok: ok = self.set_ofName()
-            if ok: ok = self.set_semester()
-            if ok: ok = self.set_prog_info(progData)
-            if ok: ok = self.set_propint(progData)
-            if ok: ok = self.set_datlevel(0)
-            if ok: ok = self.set_image_stats_keywords()
-            if ok: ok = self.set_weather_keywords()
-            if ok: ok = self.set_oa()
-            if ok: ok = self.set_npixsat(65535)
-
-            if ok: ok = self.set_wavelengths()
-            if ok: ok = self.set_specres()
-            if ok: ok = self.set_slit_dims()
-            if ok: ok = self.set_spatscal()
-            if ok: ok = self.set_dispscal()
-
-            if ok: ok = self.set_dqa_vers()
-            if ok: ok = self.set_dqa_date()
-            return ok
+        super().__init__(instr, utDate, rootDir, log)
 
 
-    def get_dir_list(self):
+        # Set any unique keyword index values here
+        # self.keywordMap['OFNAME']       = 'OFNAME'        
+        # self.keywordMap['FRAMENO']      = 'FRAMENUM'
+
+
+        # Other vars that subclass can overwrite
+        self.endTime = '20:00:00'   # 24 hour period start/end time (UT)
+
+
+        # Generate the paths to the NIRES datadisk accounts
+        self.sdataList = self.get_dir_list()
+
+
+    def run_dqa_checks(self, progData):
+        '''
+        Run all DQA checks unique to this instrument.
+        '''
+
+        # todo: check that all of these do not need a subclass version if base class func was used.
+        ok = True
+        if ok: ok = self.set_instr()
+        if ok: ok = self.set_dateObs()
+        if ok: ok = self.set_utc()
+        if ok: ok = self.set_koaimtyp()
+        if ok: ok = self.set_koaid()
+        if ok: ok = self.set_ut()
+        if ok: ok = self.set_frameno()
+        if ok: ok = self.set_ofName()
+        if ok: ok = self.set_semester()
+        if ok: ok = self.set_prog_info(progData)
+        if ok: ok = self.set_propint(progData)
+        if ok: ok = self.set_datlevel(0)
+        if ok: ok = self.set_image_stats_keywords()
+        if ok: ok = self.set_weather_keywords()
+        if ok: ok = self.set_oa()
+        if ok: ok = self.set_npixsat(65535)
+
+        # if ok: ok = self.set_wavelengths()
+        # if ok: ok = self.set_specres()
+        # if ok: ok = self.set_slit_dims()
+        # if ok: ok = self.set_spatscal()
+        # if ok: ok = self.set_dispscal()
+
+        if ok: ok = self.set_dqa_vers()
+        if ok: ok = self.set_dqa_date()
+        return ok
+
+
+    @staticmethod
+    def get_dir_list():
         '''
         Function to generate the paths to all the KCWI accounts, including engineering
         Returns the list of paths
         '''
+
+        #TODO: verify that /sdata1401/ is not used
+        #todo: verify that /kcwidev/ is not used
         dirs = []
-        path = '/s/sdata1400/kcwi'
-        for i in range(1,10):
-            joinSeq = (path, str(i))
-            path2 = ''.join(joinSeq)
-            dirs.append(path2)
-        joinSeq = (path, 'dev')
-        path2 = ''.join(joinSeq)
-        dirs.append(path2)
+        path = '/s/sdata140'
+        for i in range(0,1):
+            path2 = path + str(i) + '/kcwi'
+            for j in range(1,10):
+                path3 = path2 + str(j)
+                dirs.append(path3)
+            dirs.append(path2 + 'dev')
+            dirs.append(path2 + 'eng')
         return dirs
 
-    def get_prefix(self, keys):
-        instr = self.get_instr(keys)
+
+    def get_prefix(self):
+
+        instr = self.get_instr()
         if instr == 'kcwi':
-            try:
-                camera = keys['CAMERA'].lower()
-            except KeyError:
-                prefix = ''
-            if camera == 'blue':
-                prefix = 'KB'
-            elif camera == 'red':
-                prefix = 'KR'
-            elif camera == 'fpc':
-                prefix = 'KF'
-            else:
-                prefix = ''
+            camera = self.get_keyword('CAMERA', default='')
+            camera = camera.lower()
+            if   camera == 'blue': prefix = 'KB'
+            elif camera == 'red' : prefix = 'KR'
+            elif camera == 'fpc' : prefix = 'KF'
+            else                 : prefix = ''
         else:
             prefix = ''
         return prefix
+
+
+    def set_koaimtyp(self):
+        """
+        Uses get_koaimtyp to set KOAIMTYP
+        """
+
+        #self.log.info('set_koaimtyp: setting KOAIMTYP keyword value')
+
+        koaimtyp = self.get_koaimtyp()
+
+        # Warn if undefined
+        if koaimtyp == 'undefined':
+            self.log.info('set_koaimtyp: Could not determine KOAIMTYP value')
+
+        # Update keyword
+        self.set_keyword('KOAIMTYP', koaimtyp, 'KOA: Image type')
+
+        return True
+
+
+    def get_koaimtyp(self):
+        """
+        Determine image type based on instrument keyword configuration
+        """
+
+        # Default KOAIMTYP value
+        koaimtyp = 'undefined'
+
+
+        #todo
+
+
+        return koaimtyp
+
+
+    def get_dispmode(self, update=False):
+        """
+        """
+
+        dispmode = ''
+
+        #todo: 
+
+        return dispmode
+
 
