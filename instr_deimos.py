a8637d97f6ea8948a6e6e9d0f267f15396f8664e
Jeff Mader <jmader@vm-koaserver2.(none)>
2021-01-13 14:44:07 -1000

    Removed set_filesize(), now called for all instruments

--- instr_deimos.py
+++ instr_deimos.py
@@ -106,7 +106,6 @@ class Deimos(instrument.Instrument):
         if ok: ok = self.set_spatscal()
         if ok: ok = self.set_dispscal()
         if ok: ok = self.set_specres()
-        if ok: ok = self.set_filesize()
 
         return ok
 
