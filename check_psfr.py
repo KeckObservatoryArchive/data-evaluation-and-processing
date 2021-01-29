ef1519ecc90f75c36253c4af237d92bef43dd725
Josh Riley <joshjriley@gmail.com>
2021-01-29 12:07:32 -1000

    Used logging module instead of print

--- check_psfr.py
+++ check_psfr.py
@@ -4,7 +4,7 @@ Transfer script lives on psfrdataserver1 and this script calls it via ssh+authke
 Transfer script path is defined in pyDEP config as [INSTR]['PSFR_XFR']
 This is meant to be run on a cron once each morning for each PSFR instrument:
 
-  30  8 * * * /usr/local/anaconda3-5.0.0.1/bin/python /kroot/archive/dep/default/check_psfr NIRC2  `date -u +\%Y-\%m-\%d` >> ~/log/check_psfr.log
+  30  8 * * * /usr/local/anaconda3-5.0.0.1/bin/python /kroot/archive/dep/default/check_psfr NIRC2  `date -u +\%Y-\%m-\%d`
 
 Overview of PSFR archive process:
 - PSFR processing is activated by overriding the Instrument.run_psfr() function for an instrument in pyDEP.
@@ -31,10 +31,9 @@ import argparse
 import traceback
 import yaml
 import subprocess
+import logging
+from pathlib import Path
 
-
-#todo: use logger module?
-#todo: wait for exit status and log/report output
 
 def main():
 
@@ -48,7 +47,12 @@ def main():
     instr  = args.instr.upper()
     utdate = args.utdate
     dev    = args.dev
-    print(f"Running {sys.argv}")
+
+    #create logfile
+    outdir = '/tmp' if dev else '/home/koaadmin/log'
+    create_logger('check_psfr', outdir)
+    log = logging.getLogger('check_psfr')
+    log.info(f"Running {sys.argv}")
 
     #cd to script dir so relative paths work
     os.chdir(sys.path[0])
@@ -73,55 +77,57 @@ def main():
 
     #No results?
     if not rows:
-        print(f"No PSFR record for {instr} {utdate}")
+        log.info(f"No PSFR record for {instr} {utdate}")
         return
     row = rows[0]
 
     # Check if no PSFR files
     if not row['files']:
-        print(f"No PSFR files for {instr} {utdate}")
+        log.info(f"No PSFR files for {instr} {utdate}")
         return
 
     # See if it is still processing
     if not row['end_time']:
-        print(f"Record still processing for {instr} {utdate}")
+        log.info(f"Record still processing for {instr} {utdate}")
         return
 
     # See if it was already sent
     if row['ingest_stat'] == 'DONE':
-        print(f"Record already ingested for {instr} {utdate}")
+        log.info(f"Record already ingested for {instr} {utdate}")
         return
 
     #execute command
     cmd = psfr_xfr.split();
     cmd += [instr, utdate, row['location']]
-    print('Command: ', ' '.join(cmd))
+    log.info('Command: ', ' '.join(cmd))
     if dev:
-        print("DEV MODE: NOT EXECUTING COMMAND")
+        log.info("DEV MODE: NOT EXECUTING COMMAND")
     else:
-        print("Executing command")
+        log.info("Executing command")
         out, stat = run_cmd(cmd)
         if stat != 0:
             email_admin(f"Command returned status {stat}.\nOutput:\n{out}")
             return
-    print("DONE")
+    log.info("DONE")
 
 
 def run_cmd(cmd):
     '''Run command and get output and return code.'''
+    log = logging.getLogger('check_psfr')
     try:
         ps = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         out = ps.communicate()[0]
         out = out.decode("utf-8").strip()
     except Exception as e:
-        print(traceback.format_exc())
+        log.info(traceback.format_exc())
         return None, -1
     return out, ps.returncode
 
 
 def email_admin(msg):
 
-    print(msg)
+    log = logging.getLogger('check_psfr')
+    log.info(msg)
 
     with open('config.live.ini') as f: 
         config = yaml.safe_load(f)
@@ -138,6 +144,36 @@ def email_admin(msg):
     s = smtplib.SMTP('localhost')
     s.send_message(em)
     s.quit()
+
+
+def create_logger(name, outdir='/tmp'):
+
+    #create directory if it does not exist
+    try:
+        Path(outdir).mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        print(f"ERROR: Unable to create logger dir at {outdir}.  Error: {str(e)}")
+        return False
+
+    log = logging.getLogger(name)
+    log.setLevel(logging.INFO)
+
+    # Create a file handler
+    logfile = f"{outdir}/{name}.log"
+    handle = logging.FileHandler(logfile)
+    handle.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+    handle.setFormatter(formatter)
+    log.addHandler(handle)
+
+    #add stdout to output so we don't need both log and print statements
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s - %(message)s')
+    sh.setFormatter(formatter)
+    log.addHandler(sh)
+    
+    return log
 
 
 if __name__ == "__main__":
